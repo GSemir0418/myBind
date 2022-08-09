efe5dfe58dffce58f486926623e95fb029aa7d97
gsemir0418 <845217811@qq.com>
2022-08-09 09:51:08 +0800

    polyfill myBind

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-function myBind(newThis, ...args1) {
-  const fn = this;
+var slice = Array.prototype.slice;
+function myBind(newThis) {
+  var args = slice.call(arguments, 1);
+  var fn = this;
+  if (typeof fn !== "function") throw new Error("bind必须调用在函数上");
   return function (...args2) {
-    return fn.call(newThis, ...args1, ...args2);
+    var args2 = slice.call(arguments, 0);
+    return fn.apply(newThis, args.concat(args2));
   };
 }
 module.exports = myBind;
